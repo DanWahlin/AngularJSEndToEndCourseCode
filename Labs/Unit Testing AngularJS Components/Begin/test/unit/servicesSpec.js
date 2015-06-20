5e948d9a99e9e4940b8691625e79adb1f81285da
Dan Wahlin <dwahlin@xmlforasp.net>
2015-06-19 23:31:12 -0700

    Updates to function names in latest Jasmine.

--- Labs/Unit Testing AngularJS Components/Begin/test/unit/servicesSpec.js
+++ Labs/Unit Testing AngularJS Components/Begin/test/unit/servicesSpec.js
@@ -13,23 +13,26 @@ describe('reminderFactory tests', function (){
       factory = reminderFactory;
     });
 	
-	  var store = {reminder1:'reminder1',reminder2:'reminder2',reminder3:'reminder3'};
+	var store = {reminder1:'reminder1',reminder2:'reminder2',reminder3:'reminder3'};
 
-	  spyOn(localStorage, 'getItem').andCallFake(function (key) {
-		  return store[key];
-	  });
-	  spyOn(localStorage, 'setItem').andCallFake(function (key, value) {
-		  return store[key] = value + '';
-	  });
-	  spyOn(localStorage, 'clear').andCallFake(function () {
-		  store = {};
-	  });
-    spyOn(Object, 'keys').andCallFake(function (key) {
-  		var keys=[];
+	spyOn(localStorage, 'getItem').and.callFake(function (key) {
+		return store[key];
+	});
+
+	spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+		return store[key] = value + '';
+	});
+
+	spyOn(localStorage, 'clear').and.callFake(function () {
+		store = {};
+	});
+
+    spyOn(Object, 'keys').and.callFake(function (key) {
+        var keys=[];
   		for(var key in store)
   			keys.push(key);
   		return keys;
-	  });
+	});
 
   });
      
